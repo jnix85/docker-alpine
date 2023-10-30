a6bf41dd96e9877616c18314ffbdf678a52682a2
Jason Parks <>
2023-10-30 17:05:28 -0500

    .

--- update.sh
+++ update.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "${LOCALBUILD}" == "true" ]
-then nexusRepo
-fi
-
-nexusRepo() {
 sed -i 's/dl-cdn.alpinelinux.org/nexus.home.jpconsulted.com\/repository/g' /etc/apk/repositories
-apk add --no-cache bash
-apk upgrade --no-cache
+apk add bash
+apk upgrade
+apk cache purge
+apk cache clean
 sed -i 's/nexus.home.jpconsulted.com\/repository/dl-cdn.alpinelinux.org/g' /etc/apk/repositories
-}
-
-
