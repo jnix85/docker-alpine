dfc5afa6817adba2d95b425cf59a950fa06716ba
Jason Parks <>
2023-10-30 17:07:30 -0500

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-for i in $(seq 12 18)
+for i in $(seq 13 18)
 do git checkout 3.${i}
 docker buildx build --build-arg LOCALBUILD=true --platform linux/amd64,linux/arm64 --pull --push -t registry.pupgizmo.com/alpine/alpine:3.${i} -f Dockerfile . 
 done
